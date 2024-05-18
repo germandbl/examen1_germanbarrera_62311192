ca88960702f35a509264297352bddb0c0822488e
germandbl <gdb.lopez@outlook.com>
2024-05-17 19:10:16 -0600

    LibraryScreen y VillainsScreen creadas

---
+++ lib/presentation/screens/villains_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VillainsScreen extends StatelessWidget {
+  const VillainsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
